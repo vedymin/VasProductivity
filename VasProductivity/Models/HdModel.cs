07e3dda8675fb1f5357e16eab4b5f10638063759
Dawid <michalskimlm@gmail.com>
2018-12-03 13:56:44 +0100

    Rename ScannedHdRecordModel to HdModel

---
+++ VasProductivity/Models/HdModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VAS_Prod
+{
+	public class HdModel
+	{
+		public int id { get; set; }
+		public string HD { get; set; }
+		public string pack_station { get; set; }
+		public int pcs { get; set; }
+		public DateTime date_time { get; set; }
+
+		public void GetQuantityOfHdFromReflex()
+		{
+			pcs = DataAccessModel.GetQuantityOfPiecesInHd(HD).pcs;
+			if (pcs == 1)
+			{
+				pcs = DataAccessModel.GetQuantityOfComponentsInHd(HD).pcs;
+			}
+		}
+
+		internal bool CheckIfHdIsAlreadyScannedInMySql()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
